e1b274d0a3f24383e1008710a731dc884ba7e15b
Shandon Mith <Shandonmith@gmail.com>
2022-08-03 15:25:01 -0400

    contact section done

--- app.js
+++ app.js
@@ -1,38 +1,68 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll("controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button");
+const modalText = document.getElementById("modal-txt");
 
+function validateForm() {
+  modalText.innerHTML =
+    "Thank you " +
+    document.forms["contactForm"]["name"].value +
+    " for your submission, I will be in contact with your shortly!";
+  modal.classList.toggle("show-modal");
+  return true;
+}
 
-function PageTranstions() {
+function PageTransitions() {
   // Button click active class
-  for (let i=0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener('click', function() {
-      let currentBtn = document.querySelectorAll('.active-btn');
-      currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-      this.className += ' active-btn';
-    })
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
   }
 
   //Section active class
-  allSections.addEventListener('click', (e) => {
+  allSections.addEventListener("click", (e) => {
     const id = e.target.dataset.id;
     if (id) {
       //remove selected from the other btns
       sectBtn.forEach((btn) => {
-        btn.classList.remove('active')
-      })
-      e.target.classList.add('active')
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
 
       // hide other sections
       sections.forEach((section) => {
-        section.classList.remove('active')
-      })
+        section.classList.remove("active");
+      });
 
       const element = document.getElementById(id);
-      element.classList.add('active');
+      element.classList.add("active");
     }
-  })
+  });
+
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
+
+  closeButton.addEventListener("click", () => {
+    modal.classList.toggle("show-modal");
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.classList.toggle("show-modal");
+    }
+  });
 }
 
-PageTranstions();
+PageTransitions();
